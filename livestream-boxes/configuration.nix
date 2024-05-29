eaf6d211d2ea1a0b43f89d924c501f20d45f3cf7
Qumarth Jash <qumarthjash@gmail.com>
2024-05-29 15:07:54 +0100

    Switch to gnome cause the plasma greeter is crashing

--- livestream-boxes/configuration.nix
+++ livestream-boxes/configuration.nix
@@ -37,8 +37,8 @@
   services.xserver.enable = true;
 
   # Enable the Plasma 5 Desktop Environment.
-  services.xserver.displayManager.sddm.enable = true;
-  services.xserver.desktopManager.plasma5.enable = true;
+  services.xserver.displayManager.gdm.enable = true;
+  services.xserver.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   # services.xserver.xkb.layout = "us";
